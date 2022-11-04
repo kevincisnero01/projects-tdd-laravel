aa2a91cc3b09c46bdb8dfd6f2ffbee82bd9cb205
kevincisnero01 <7884578+kevincisnero01@users.noreply.github.com>
2022-11-03 21:33:25 -0400

    Diseño con Tailwind CSS

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,9 +13,25 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body class="bg-gray-200">
-        @foreach ($repositories as $repository)
-            <h2>{{ $repository->url }}</h2>
-            <p>{{ $repository->description }}</p>
-        @endforeach
+        <h1 class="my-4 mx-auto text-4xl font-weight text-center">Proyectos</h1>
+        <ul class="w-full bg-white border-r border-gray-300 shadow-xl">
+            @foreach ($repositories as $repository)
+                <li class="flex items-center text-black p-2 hover:bg-gray-300">
+                    <img 
+                        src="{{ asset($repository->user->profile_photo_url) }}" 
+                        class="w-12 h-12 rounded-full mr-2"
+                    >
+                    <div class="flex justify-between w-full">
+                        <div class="flex-1">
+                            <h2 class="text-sm font-semibold text-gray-900">{{ $repository->url }}</h2>
+                            <p>{{ $repository->description }}</p>
+                        </div>
+                        <span class="text-xs font-medium text-gray-600">
+                            {{ $repository->created_at->diffForHumans() }}
+                        </span>
+                    </div>
+                </li>
+            @endforeach
+        </ul>
     </body>
 </html>
