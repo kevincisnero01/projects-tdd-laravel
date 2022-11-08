b9e91bf5464cd03abd5198fc3969fc5a08379571
kevincisnero01 <7884578+kevincisnero01@users.noreply.github.com>
2022-11-07 23:32:39 -0400

    Ajustes Finales
    -Botones  faltantes
    -Estilo boton
    -Confirm eliminar

--- resources/views/repositories/index.blade.php
+++ resources/views/repositories/index.blade.php
@@ -7,6 +7,13 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <p class="text-right mb-4">
+                <a href="{{ route('repositories.create') }}"
+                   class="bg-blue-500 text-white font-bold py-2 px-4 rounded-md text-xs"
+                >
+                    Agregar Repositorio
+                </a>
+            </p>
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-4">
             <table>
                 <head>
@@ -23,10 +30,18 @@
                         <td class="border px-4 py-2">{{ $repository->id }}</td>
                         <td class="border px-4 py-2">{{ $repository->url }}</td>
                         <td>
-                            <a href="{{ route('repositories.show', $repository) }}" class="mx-4">Ver</a>
+                            <a href="{{ route('repositories.show', $repository) }}" class="mx-4 px-4 rounded-md bg-gray-500 text-white">Ver</a>
                         </td>
                         <td>
-                            <a href="{{ route('repositories.edit', $repository) }}" class="mx-4">Editar</a>
+                            <a href="{{ route('repositories.edit', $repository) }}" class="mx-4 px-4 rounded-md bg-sky-500 text-white">Editar</a>
+                        </td>
+                        <td class="px-4 py-2">
+                            <form action="{{ route('repositories.destroy', $repository) }}" method="POST" onSubmit="return confirm('Desea eliminar el repositorio?')" >
+                                @csrf
+                                @method('DELETE')
+                                
+                                <input type="submit" value="Eliminar" class="px-4 rounded-md bg-red-500 text-white">
+                            </form>
                         </td>
                     </tr>
                     @empty
